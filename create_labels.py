d9a2b31c45fb0953241a92803781c602cef50711
Harry <39486482+harrydlr@users.noreply.github.com>
2023-10-09 03:14:47 +0200

    Update create_labels.py

--- create_labels.py
+++ create_labels.py
@@ -4,7 +4,6 @@ import os
 if __name__ == "__main__":
     data_dir = '/home/harry/Downloads/test-clean/'
     model_dir = 'speechbrain/vad-crdnn-libriparty'
-    visualize = False  # Set to True if you want to visualize predictions
     labels_dir = '/home/harry/Downloads/labels'  # Modify this path as needed
 
     labeler = audio_labeller_vf.VADLabeler(model_dir, labels_dir)
